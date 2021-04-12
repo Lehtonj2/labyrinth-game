d6406d0cfc3db40e319a915b0ccc5e5cd9439a25
Jussi Lehtonen <jussi.j.lehtonen@aalto.fi>
2021-04-12 20:29:52 +0300

    Added primary stage to GUI class

--- src/Game/Main/GUI.scala
+++ src/Game/Main/GUI.scala
@@ -1,7 +1,22 @@
 package Game.Main
 
 import scalafx.application.JFXApp
+import scalafx.scene.Scene
 
 
 
-object Main extends JFXApp
+
+object Main extends JFXApp {
+val root = new Scene
+
+    stage = new JFXApp.PrimaryStage {
+        title.value = "Labyrinth-game"
+        height = 400
+        width = 600
+        scene = root
+    }
+}
+
+
+
+
