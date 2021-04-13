68d00a4c3099496f451318e9fef7686ab44360cd
Jussi Lehtonen <jussi.j.lehtonen@aalto.fi>
2021-04-13 19:41:16 +0300

    Developed Labyrinth and Grid classes.

--- src/Game/Main/Grid.scala
+++ src/Game/Main/Grid.scala
@@ -1,8 +1,23 @@
 package Game.Main
+import scala.collection.mutable.Buffer
 
 class Grid(width: Int, height: Int) {
-  private val y = (0 to height).toArray
-  private val x = (0 to width).toArray
-  val locations = y.map(n => x.map(m => (m, n)))
+  val gridSize = width * height
+  private val y = (0 until height).toArray
+
+  private val x = (0 until width).toArray
+
+  val locations = x.flatMap(n => y.map(m => (n, m))).toBuffer
+
+  def contains(x:Int, y: Int) = x >= 0 && x < this.width && y >= 0 && y < this.height
+
+  def neighbours(x: Int, y: Int) = {
+    val neighbourLocations = Buffer[(Int, Int)]()
+    if (contains(x, y - 1)) neighbourLocations.+=:(x, y - 1)
+    if (contains(x, y + 1)) neighbourLocations.+=:(x, y + 1)
+    if (contains(x - 1, y)) neighbourLocations.+=:(x - 1, y)
+    if (contains(x + 1, y)) neighbourLocations.+=:(x + 1, y)
+    neighbourLocations
+  }
 
 }
--- src/Game/Main/Labyrinth.scala
+++ src/Game/Main/Labyrinth.scala
@@ -1,8 +1,12 @@
 package Game.Main
-
+import scala.collection.mutable.Buffer
 
 class Labyrinth {
   def createLabyrinth(grid: Grid) = {
+    val labyrinthSize = grid.gridSize
+    val visitedCells = Buffer[(Int, Int)]()
+    val floorCells = Buffer[(Int, Int)]()
+
 
   }
 
