704f6ceaaf8c7cdbcf77c755a9e68022e1d0a060
Jussi Lehtonen <jussi.j.lehtonen@aalto.fi>
2021-04-20 22:43:21 +0300

    Developed Labyrinth and GUI classes.

--- src/Game/Main/GUI.scala
+++ src/Game/Main/GUI.scala
@@ -1,21 +1,45 @@
 package Game.Main
 
+
+import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
-
-
+import scalafx.scene.paint.Color._
+import scalafx.scene.shape.Rectangle
+import scalafx.scene.layout.Pane
+import scala.collection.mutable.Buffer
 
 
 
 object Main extends JFXApp {
-val root = new Scene
 
     stage = new JFXApp.PrimaryStage {
         title.value = "Labyrinth-game"
         height = 400
         width = 400
-        scene = root
     }
+
+    val root = new Pane //Simple pane component
+    val scene = new Scene(root) //Scene acts as a container for the scene graph
+    stage.scene = scene
+
+    val grid = new Grid(10, 10)
+
+    val labyrinth = new Labyrinth
+    val floors = labyrinth.createLabyrinth(grid)
+    val floorRectangles = Buffer[Rectangle]()
+    for (i <- floors) {
+    val rectangle = new Rectangle {
+    x = i._1 * 15
+    y = i._2 * 15
+    width = 10
+    height = 10
+    fill = Blue //scalafx.scene.paint.Color
+}
+    floorRectangles += rectangle
+
+    }
+    floorRectangles.foreach(n => root.children += n)
 
 }
 
--- src/Game/Main/Labyrinth.scala
+++ src/Game/Main/Labyrinth.scala
@@ -1,13 +1,29 @@
 package Game.Main
 import scala.collection.mutable.Buffer
+import scala.util.Random
 
 class Labyrinth {
   def createLabyrinth(grid: Grid) = {
     val labyrinthSize = grid.gridSize
     val visitedCells = Buffer[(Int, Int)]()
     val floorCells = Buffer[(Int, Int)]()
+    val firstCell = grid.locations(Random.nextInt(labyrinthSize))
+    floorCells += firstCell
+    visitedCells += firstCell
 
-
+    while (floorCells.nonEmpty) {
+      val next = floorCells.reverse.head
+      val neighbours = grid.neighbours(next._1, next._2).filter(n => !visitedCells.contains(n))
+        if (neighbours.nonEmpty) {
+          val neighbour = neighbours(Random.nextInt(neighbours.size))
+          floorCells += neighbour
+          visitedCells += neighbour
+        }else{
+          floorCells -= next
+        }
+      }
+      println(visitedCells)
+      visitedCells
   }
 
 }
